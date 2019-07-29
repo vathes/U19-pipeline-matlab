a391c412653c37ab2c2ed7c4dae10321cc10ab56
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2019-07-29 15:19:12 -0400

    nicer progress indicator in example_NewfieldUpdate.m

--- schemas_matlab/example_NewfieldUpdate.m
+++ schemas_matlab/example_NewfieldUpdate.m
@@ -7,15 +7,24 @@ addAttribute(acquisition.TowersBlock, 'block_performance: float  # performance i
 
 %% update one field in TowersBlock
 allBlocks = fetch(acquisition.TowersBlock); % Get the primary keys of all Blocks
-
+reverseStr = '';
 for key_idx = 1:length(allBlocks)
     key = allBlocks(key_idx);
-    disp(key_idx/length(allBlocks))
     trials = fetch(acquisition.TowersBlockTrial & key, '*'); % Get trials in blocks
     correct_counter = 0;
     for i = 1:length(trials)
         correct_counter = correct_counter + strcmp(trials(i).trial_type, trials(i).choice);
     end
     performance = correct_counter / length(trials);          % Calculate performance
-    update(acquisition.TowersBlock & key, 'block_performance', performance) % Update entry
+    if isfinite(performance)
+        update(acquisition.TowersBlock & key, 'block_performance', performance); % Update entry
+    else
+        update(acquisition.TowersBlock & key, 'block_performance', 0);
+    end
+    
+    % Nice progress indicator
+    percentDone = 100*key_idx/length(allBlocks);
+    msg = sprintf('Percent done: %3.1f', percentDone);
+    fprintf([reverseStr, msg]);
+    reverseStr = repmat(sprintf('\b'), 1, length(msg));
 end
