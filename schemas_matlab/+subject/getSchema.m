73949b64139452860d30c81a27c79d1795017c21
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2019-08-26 16:39:01 -0400

    update to U19

--- schemas_matlab/+subject/getSchema.m
+++ schemas_matlab/+subject/getSchema.m
@@ -1,7 +1,7 @@
 function obj = getSchema
 persistent schemaObject
 if isempty(schemaObject)
-    schemaObject = dj.Schema(dj.conn, 'subject', 'pni_subject');
+    schemaObject = dj.Schema(dj.conn, 'subject', 'U19_subject');
 end
 obj = schemaObject;
 end
