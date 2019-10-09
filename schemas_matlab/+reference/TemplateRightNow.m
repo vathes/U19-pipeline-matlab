e898bc1648df800b7234bdf129152d807b4613c2
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2019-10-09 18:10:39 -0400

    plot index added for correct display in the GUI

--- schemas_matlab/+reference/TemplateRightNow.m
+++ schemas_matlab/+reference/TemplateRightNow.m
@@ -1,7 +1,9 @@
 %{
 # ActionItems template for the information of each column
 -> reference.Template
-data :     varchar(64)     
+data                        : varchar(64)                   # 
+---
+plot_index                  : int                           # index of where to plot in GUI
 %}
 
 classdef TemplateRightNow < dj.Part
