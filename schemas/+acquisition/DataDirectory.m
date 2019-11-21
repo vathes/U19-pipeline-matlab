85e9cf1c4ea0667e217a2f19dbfa8d30f1eb9e8a
shenshan <shenshanpku@gmail.com>
2019-11-21 11:14:43 -0800

    rename variable subject to subj, to avoid conflict

--- schemas/+acquisition/DataDirectory.m
+++ schemas/+acquisition/DataDirectory.m
@@ -16,27 +16,26 @@ classdef DataDirectory < dj.Computed
 
 		function makeTuples(self, key)
             
-            info = subject.Subject * acquisition.Session & key;
-            [user, rig, subject, session_date] = fetch1(...
-                info, 'user_id', 'session_location', 'subject_nickname', 'session_date');
+            [user, rig, subj, session_date] = fetch1(...
+                subject.Subject * acquisition.Session & key, 'user_id', 'session_location', 'subject_nickname', 'session_date');
             session_date = erase(session_date, '-');  
 
             if strcmp('Bezos3', rig)
                 % Or on the scope in bezos
                 base_dir = '/braininit/RigData/scope/bay3/';
-            	file = dir(getLocalPath([base_dir '/' user '/*/data/' subject '/*_' session_date '.mat']));
+            	file = dir(getLocalPath([base_dir '/' user '/*/data/' subj '/*_' session_date '.mat']));
             else
                 % Training rigs 1 to 7
                 rig_number = rig(regexp(rig, '[0-9]'));
                 base_dir = '/braininit/RigData/training/';
-                if strcmp(subject, 'E86')
-                    subject = 'e86';
+                if strcmp(subj, 'E86')
+                    subj = 'e86';
                 end
             	file = dir(getLocalPath([base_dir 'rig' rig_number '/' user '/*/data/' subject '/*_' session_date '.mat']));
             end
             if strcmp('VRLaser', rig)
                 base_dir = '/braininit/RigData/VRLaser/behav/lucas/blocksReboot/data';
-                file = dir(getLocalPath([base_dir '/' subject '/*_' session_date '.mat']));
+                file = dir(getLocalPath([base_dir '/' subj '/*_' session_date '.mat']));
             end
             
             if isempty(file)
