1f2147bf2606b9a235a09917891964016138957a
shenshan <shenshanpku@gmail.com>
2019-11-21 12:30:00 -0800

    Change the prefix to be u19_

--- startup.m
+++ startup.m
@@ -1,12 +1,6 @@
 
 addpath(genpath('../../pipelines'))
-
-if strcmp(computer, 'MACI64')
-    setenv('DB_PREFIX', 'u19_')
-else
-    setenv('DB_PREFIX', 'shans_')
-end
-
+setenv('DB_PREFIX', 'u19_')
 
 host = env('DJ_HOST');
 user = env('DJ_USER');
