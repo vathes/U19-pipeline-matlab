a33778bfbf7ab342d0e4f6320f6c4fbaa131934c
shenshan <shenshanpku@gmail.com>
2020-01-23 19:58:58 -0800

    MATLAB tutorial

---
+++ tutorials/202001/+tutorial/Spikes.m
@@ -0,0 +1,34 @@
+%{
+-> tutorial.Neuron
+-> tutorial.SpikeDetectionParam
+-----
+spikes: longblob   # detected spikes
+count: int         # total number of the detected spikes
+%}
+
+classdef Spikes < dj.Computed
+    
+    properties
+        popRel = tutorial.Neuron * tutorial.SpikeDetectionParam & 'sdp_id=4'
+    end
+
+	methods(Access=protected)
+
+		function makeTuples(self, key)
+            
+            activity = fetch1(tutorial.Neuron & key, 'activity');
+            threshold = fetch1(tutorial.SpikeDetectionParam & key, 'threshold');
+            
+            above_thres = activity > threshold;
+            rising = diff(above_thres) > 0;
+            spikes = [0, rising];
+            count = sum(spikes);
+            
+            % save results and insert
+            key.spikes = spikes;
+            key.count = count;
+			self.insert(key)
+		end
+	end
+
+end
