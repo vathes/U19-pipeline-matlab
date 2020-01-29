fb5dcc489faef42727258051d0d168be43d76cde
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2020-01-29 14:04:38 -0500

    small updates to action table, so that no del() is neccessary when updating entries

--- schemas/+subject/SubjectActionAutomatic.m
+++ schemas/+subject/SubjectActionAutomatic.m
@@ -3,6 +3,7 @@
 notification_date:     datetime        # datetime when notification was automatically generated
 ---
 notification_message:  varchar(255)    # Notification message e.g. low bodyweight warning
+valid_until_date:      datetime        # datetime when notification was inactivated
 %}
 
 classdef SubjectActionAutomatic < dj.Manual
--- schemas/+subject/SubjectActionManual.m
+++ schemas/+subject/SubjectActionManual.m
@@ -1,6 +1,9 @@
 %{
 -> Subject
 -> ActItem
+---
+notification_date:     datetime        # datetime when notification was generated
+valid_until_date:      datetime        # datetime when notification was inactivated
 %}
 
 classdef SubjectActionManual < dj.Manual
