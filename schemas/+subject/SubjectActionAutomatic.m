9e27b9e8d3e1e5eba536f2735d7e84bc9235eb62
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2020-01-29 19:04:08 -0500

    updates to action tables to avoid the del() problem for technicians without drop privileges.

--- schemas/+subject/SubjectActionAutomatic.m
+++ schemas/+subject/SubjectActionAutomatic.m
@@ -1,9 +1,9 @@
 %{
--> Subject
-notification_date:     datetime        # datetime when notification was automatically generated
+-> subject.Subject
+notification_date = '1900-01-01 00:00:00':     datetime        # datetime when notification was automatically generated
 ---
-notification_message:  varchar(255)    # Notification message e.g. low bodyweight warning
-valid_until_date:      datetime        # datetime when notification was inactivated
+notification_message:       varchar(255)    # Notification message e.g. low bodyweight warning
+valid_until_date=null:      datetime        # datetime when notification was inactivated
 %}
 
 classdef SubjectActionAutomatic < dj.Manual
--- schemas/+subject/SubjectActionManual.m
+++ schemas/+subject/SubjectActionManual.m
@@ -1,9 +1,9 @@
 %{
--> Subject
--> ActItem
+-> subject.Subject
+-> subject.ActItem
+notification_date = '1900-01-01 00:00:00':     datetime        # datetime when notification was generated
 ---
-notification_date:     datetime        # datetime when notification was generated
-valid_until_date:      datetime        # datetime when notification was inactivated
+valid_until_date  = null:      datetime        # datetime when notification was inactivated
 %}
 
 classdef SubjectActionManual < dj.Manual
