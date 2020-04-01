3365f5f638fbe3f2c3e0ca7564f369194d84d6cc
lpinto <lpinto@princeto.edu>
2020-04-01 16:48:07 -0400

    resolution_xy is a blob not a float

--- schemas/+meso/FieldOfView.m
+++ schemas/+meso/FieldOfView.m
@@ -8,7 +8,7 @@ fov_depth               :  float          # depth of the field of view  should b
 fov_center_xy           :  blob           # X-Y coordinate for the center of the FOV in microns. One for each FOV in scan
 fov_size_xy             :  blob           # X-Y size of the FOV in microns. One for each FOV in scan (sizeXY)
 fov_rotation_degrees    :  float          # rotation of the FOV with respect to cardinal axes in degrees. One for each FOV in scan
-fov_pixel_resolution_xy :  float          # number of pixels for rows and columns of the FOV. One for each FOV in scan
+fov_pixel_resolution_xy :  blob           # number of pixels for rows and columns of the FOV. One for each FOV in scan
 fov_discrete_plane_mode :  boolean        # true if FOV is only defined (acquired) at a single specifed depth in the volume. One for each FOV in scan should this be boolean?
 %}
 
