b02c2b914fdc5c70c2080141f2b07e55f35b029d
lpinto <lpinto@princeto.edu>
2020-04-03 12:25:56 -0400

    typo

--- schemas/+meso/Segmentation.m
+++ schemas/+meso/Segmentation.m
@@ -241,7 +241,7 @@ function fileChunk = selectFileChunks(key,chunk_cfg)
 % fileChunk is an array of size chunks x 2, where rows are [firstFileIdx lastFileIdx]
 
 %% check if enforcing this is actually desired
-file_ids       = fetchn(meso.FiledOfViewFile & key,'file_number');
+file_ids       = fetchn(meso.FieldOfViewFile & key,'file_number');
 nfiles         = numel(file_ids);
 
 if ~chunk_cfg.auto_select_behav && ~chunk_cfg.auto_select_bleach && nfiles < chunk_cfg.filesPerChunk
