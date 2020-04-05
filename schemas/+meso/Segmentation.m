c75a1b4a8bf6d91e8ce92cb3c484d354d453c596
lpinto <lpinto@princeto.edu>
2020-04-05 11:58:36 -0400

    error is from writing all rois with insertn, presumably too much data. moved to inside the loop

--- schemas/+meso/Segmentation.m
+++ schemas/+meso/Segmentation.m
@@ -110,7 +110,6 @@ classdef Segmentation < dj.Imported
       end
       
       %% load summary file
-      keyboard
       data                                 = load(outputFiles{1});
       num_chunks                           = numel(data.chunk);
       result.num_chunks                    = num_chunks;
@@ -163,76 +162,55 @@ classdef Segmentation < dj.Imported
       morpho_data   = keydata;
       trace_data    = keydata;
       
-      roi_data.roi_idx                  = [];
-      roi_data.roi_global_xy            = [];
-      roi_data.roi_spatial              = [];
-      roi_data.surround_spatial         = [];
-      roi_data.roi_is_in_chunks         = [];
-      roi_data                          = repmat(roi_data,[1 nROIs]);
       
-      morpho_data.roi_idx               = [];
-      morpho_data.morphology            = [];
-      morpho_data                       = repmat(morpho_data,[1 nROIs]);
-      
-      trace_data.roi_idx                = [];
-      trace_data.dff_roi                = [];
-      trace_data.dff_surround           = [];
-      trace_data.dff_roi_is_significant = [];
-      trace_data.dff_roi_is_baseline    = [];
-      trace_data.spiking                = [];
-      trace_data.time_constants         = [];
-      trace_data.init_concentration     = [];
-      trace_data                        = repmat(trace_data,[1 nROIs]);
-
       % loop through ROIs
       for iROI = 1:nROIs
-        roi_data(iROI).roi_idx                    = iROI;  
-        morpho_data(iROI).roi_idx                 = iROI;
-        trace_data(iROI).roi_idx                  = iROI;
+        roi_data.roi_idx                    = iROI;  
+        morpho_data.roi_idx                 = iROI;
+        trace_data.roi_idx                  = iROI;
         
-        roi_data(iROI).roi_global_xy              = globalXY(:,iROI);
-        trace_data(iROI).time_constants           = data.cnmf.timeConstants{iROI};
-        trace_data(iROI).init_concentration       = data.cnmf.initConcentration{iROI};
+        roi_data.roi_global_xy              = globalXY(:,iROI);
+        trace_data.time_constants           = data.cnmf.timeConstants{iROI};
+        trace_data.init_concentration       = data.cnmf.initConcentration{iROI};
         
         % now look in file chunks and fill activity etc
-        trace_data(iROI).dff_roi                  = nan(1,totalFrames);
-        trace_data(iROI).dff_surround             = nan(1,totalFrames);
-        trace_data(iROI).spiking                  = nan(1,totalFrames);
-        trace_data(iROI).dff_roi_is_significant   = nan(1,totalFrames);
-        trace_data(iROI).dff_roi_is_baseline      = nan(1,totalFrames);
+        trace_data.dff_roi                  = nan(1,totalFrames);
+        trace_data.dff_surround             = nan(1,totalFrames);
+        trace_data.spiking                  = nan(1,totalFrames);
+        trace_data.dff_roi_is_significant   = nan(1,totalFrames);
+        trace_data.dff_roi_is_baseline      = nan(1,totalFrames);
 
         for iChunk = 1:numel(chunkdata)
           % find roi in chunks
-          localIdx                                = data.chunk.globalID == iChunk;
+          localIdx                          = data.chunk.globalID == iChunk;
           if sum(localIdx) == 0; continue; end
-          roi_data(iROI).roi_is_in_chunks         = [roi_data(iROI).roi_is_in_chunks iChunk];
+          roi_data.roi_is_in_chunks         = [roi_data.roi_is_in_chunks iChunk];
             
           % activity traces
           frameIdx                                = chunkRange(iChunk,1):chunkRange(iChunk,2);
           uniqueData                              = chunkdata{iChunk}.cnmf.uniqueData(localIdx,:);
           uniqueBase                              = halfSampleMode(uniqueData(localIdx,:)');
           surroundData                            = chunkdata{iChunk}.cnmf.surroundData(localIdx,:);
-          trace_data(iROI).dff_roi(frameIdx)      = uniqueData / uniqueBase - 1;
-          trace_data(iROI).dff_surround(frameIdx) = surroundData / uniqueBase - 1;
-          trace_data(iROI).spiking(frameIdx)      = chunkdata{iChunk}.cnmf.spiking(localIdx,:);
-          trace_data(iROI).dff_roi_is_significant(frameIdx) = chunkdata{iChunk}.cnmf.isSignificant(localIdx,:);
-          trace_data(iROI).dff_roi_is_baseline(frameIdx)    = chunkdata{iChunk}.cnmf.isBaseline(localIdx,:);
+          trace_data.dff_roi(frameIdx)      = uniqueData / uniqueBase - 1;
+          trace_data.dff_surround(frameIdx) = surroundData / uniqueBase - 1;
+          trace_data.spiking(frameIdx)      = chunkdata{iChunk}.cnmf.spiking(localIdx,:);
+          trace_data.dff_roi_is_significant(frameIdx) = chunkdata{iChunk}.cnmf.isSignificant(localIdx,:);
+          trace_data.dff_roi_is_baseline(frameIdx)    = chunkdata{iChunk}.cnmf.isBaseline(localIdx,:);
           
           % roi: shape and morphological classification
           if isempty(roi_data(iROI).roi_spatial)
-            roi_data(iROI).roi_spatial      = reshape(chunkdata{iChunk}.cnmf.spatial(:,localIdx),chunkdata{iChunk}.cnmf.region.ImageSize);
-            roi_data(iROI).surround_spatial = reshape(chunkdata{iChunk}.cnmf.surround(:,localIdx),chunkdata{iChunk}.cnmf.region.ImageSize);
-            morpho_data(iROI).morphology    = char(chunkdata{iChunk}.cnmf.morphology(localIdx));
+            roi_data.roi_spatial      = reshape(chunkdata{iChunk}.cnmf.spatial(:,localIdx),chunkdata{iChunk}.cnmf.region.ImageSize);
+            roi_data.surround_spatial = reshape(chunkdata{iChunk}.cnmf.surround(:,localIdx),chunkdata{iChunk}.cnmf.region.ImageSize);
+            morpho_data.morphology    = char(chunkdata{iChunk}.cnmf.morphology(localIdx));
           end
         end
         
+        % insert in tables
+        inserti(meso.SegmentationRoi, roi_data)
+        inserti(meso.SegmentationRoiMorphologyAuto, morpho_data)
+        inserti(meso.Trace, trace_data)
       end
-      
-      % insert in tables
-      insert(meso.SegmentationRoi, roi_data)
-      insert(meso.SegmentationRoiMorphologyAuto, morpho_data)
-      insert(meso.Trace, trace_data)
-      
+
     end
   end
 end
