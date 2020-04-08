be5e0ccc9e6f95fb278f023f7d5bc5a9d278bc96
lpinto <lpinto@princeto.edu>
2020-04-08 11:08:53 -0400

    reverting back for now

--- schemas/+meso/getSchema.m
+++ schemas/+meso/getSchema.m
@@ -1,9 +1,9 @@
 function obj = getSchema
-% prefix = getenv('DB_PREFIX');
+prefix = getenv('DB_PREFIX');
 persistent schemaObject
 if isempty(schemaObject)
-%     schemaObject = dj.Schema(dj.conn, 'meso', [prefix 'meso']);
-  schemaObject = dj.Schema(dj.conn, 'external', 'external');
+  schemaObject = dj.Schema(dj.conn, 'meso', [prefix 'meso']);
+%   schemaObject = dj.Schema(dj.conn, 'external', 'external');
 end
 obj = schemaObject;
 end
