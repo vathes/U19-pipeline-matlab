4f796f837af8dbfdf08d67c714f1d91f54e1c512
lpinto <lpinto@princeto.edu>
2020-04-27 09:52:59 -0400

    loop insterting needs to be out of schema

--- schemas/+meso/debug2.m
+++ schemas/+meso/debug2.m
@@ -17,38 +17,38 @@ classdef debug2 < dj.Imported
   methods(Access=protected)
     function makeTuples(self, key)
       
-      %% write ROI-specific info into relevant tables
-      load  /jukebox/braininit/RigData/mesoscope/imaging/dj_debug/LP_dj_debug_data.mat
-      totalFrames = 55000;
-      chunkRange  = [8001 26000];
-      iChunk      = 1;
-      sess_idx    = 1;
-      
-      % loop through ROIs
-      for iROI = 1:nROIs
-        
-        key.sess_idx                 = sess_idx; 
-        key.roi_idx                  = iROI;  
-
-        key.time_constants           = data.cnmf.timeConstants{iROI};
-        key.init_concentration       = data.cnmf.initConcentration{iROI};
-        key.dff_roi                  = nan(1,totalFrames);
-        key.dff_surround             = nan(1,totalFrames);
-        key.spiking                  = nan(1,totalFrames);
-        key.dff_roi_is_significant   = nan(1,totalFrames);
-        key.dff_roi_is_baseline      = nan(1,totalFrames);
-
-        % activity traces
-        frameIdx                                    = chunkRange(iChunk,1):chunkRange(iChunk,2);
-        key.dff_roi(frameIdx)                = data.cnmf.dataDFF(iROI,:);
-        key.dff_surround(frameIdx)           = data.cnmf.dataBkg(iROI,:);
-        key.spiking(frameIdx)                = data.cnmf.dataDFF(iROI,:);
-        key.dff_roi_is_significant(frameIdx) = data.cnmf.isSignificant(iROI,:);
-        key.dff_roi_is_baseline(frameIdx)    = data.cnmf.isBaseline(iROI,:);
-
-        self.insert(key)
-      end
-      
+%       %% write ROI-specific info into relevant tables
+%       load  /jukebox/braininit/RigData/mesoscope/imaging/dj_debug/LP_dj_debug_data.mat
+%       totalFrames = 55000;
+%       chunkRange  = [8001 26000];
+%       iChunk      = 1;
+%       sess_idx    = 1;
+%       
+%       % loop through ROIs
+%       for iROI = 1:nROIs
+%         
+%         key.sess_idx                 = sess_idx; 
+%         key.roi_idx                  = iROI;  
+% 
+%         key.time_constants           = data.cnmf.timeConstants{iROI};
+%         key.init_concentration       = data.cnmf.initConcentration{iROI};
+%         key.dff_roi                  = nan(1,totalFrames);
+%         key.dff_surround             = nan(1,totalFrames);
+%         key.spiking                  = nan(1,totalFrames);
+%         key.dff_roi_is_significant   = nan(1,totalFrames);
+%         key.dff_roi_is_baseline      = nan(1,totalFrames);
+% 
+%         % activity traces
+%         frameIdx                                    = chunkRange(iChunk,1):chunkRange(iChunk,2);
+%         key.dff_roi(frameIdx)                = data.cnmf.dataDFF(iROI,:);
+%         key.dff_surround(frameIdx)           = data.cnmf.dataBkg(iROI,:);
+%         key.spiking(frameIdx)                = data.cnmf.dataDFF(iROI,:);
+%         key.dff_roi_is_significant(frameIdx) = data.cnmf.isSignificant(iROI,:);
+%         key.dff_roi_is_baseline(frameIdx)    = data.cnmf.isBaseline(iROI,:);
+% 
+%         self.insert(key)
+%       end
+      self.insert(key)
     end
   end
 end
