7b8f944859b350cb72e72cc33e7ee9d59ab5a8e6
lpinto <lpinto@princeto.edu>
2020-05-05 16:20:03 -0400

    forgot traces table

--- schemas/+meso/Trace.m
+++ schemas/+meso/Trace.m
@@ -2,11 +2,11 @@
 # activity traces for each ROI
 -> meso.SegmentationRoi
 ---
-dff_roi                : blob@mesoimaging  # delta f/f for each cell, 1 x nFrames. In case of chunks in segmentation, frames with no data are filled with NaN
-dff_roi_is_significant : blob@mesoimaging  # same size as dff_roi, true where transitents are significant
-dff_roi_is_baseline    : blob@mesoimaging  # same size as dff_roi, true where values correspond to baseline
-dff_surround           : blob@mesoimaging  # delta f/f for the surrounding neuropil ring
-spiking                : blob@mesoimaging  # recovered firing rate of the trace
+dff_roi                : longblob  # delta f/f for each cell, 1 x nFrames. In case of chunks in segmentation, frames with no data are filled with NaN
+dff_roi_is_significant : longblob  # same size as dff_roi, true where transitents are significant
+dff_roi_is_baseline    : longblob  # same size as dff_roi, true where values correspond to baseline
+dff_surround           : longblob  # delta f/f for the surrounding neuropil ring
+spiking                : longblob  # recovered firing rate of the trace
 time_constants         : blob              # 2 floats per roi, estimated calcium kernel time constants
 init_concentration     : float             # estimated initial calcium concentration for estimated kernel
 %}
