a758a4d28125beab5b9ceb655d809926b6783e93
Alvaro Luna <alvalunasan@gmail.com>
2020-07-28 12:42:13 -0400

    Added spock path to path table

--- schemas/+lab/Path.m
+++ schemas/+lab/Path.m
@@ -4,6 +4,7 @@ system              : enum('windows', 'mac', 'linux')
 ---
 local_path          : varchar(255)               # local computer path
 net_location        : varchar(255)               # location on the network
+spock_path          : varchar(255)               # local spock path
 description=null    : varchar(255)
 %}
 
