a71b21a8c58bbee745355ec6f32d0ea6ea5ce9da
shenshan <shenshanpku@gmail.com>
2020-10-13 16:39:38 -0700

    Redefine CumulativePsych

--- schemas/+behavior/TowersSubjectCumulativePsych.m
+++ schemas/+behavior/TowersSubjectCumulativePsych.m
@@ -1,29 +1,27 @@
 %{
--> subject.Subject
-latest_date                  : date
+-> behavior.TowersSession
 -----
 subject_delta_data=null      : blob   # num of right - num of left, x ticks for data
-subject_pright_data=null     : blob   # (%) percentage went right for each delta bin for data
+subject_pright_data=null     : blob   # percentage went right for each delta bin for data
 subject_delta_error=null     : blob   # num of right - num of left, x ticks for data confidence interval 
-subject_pright_error=null    : blob   # (%) confidence interval for precentage went right of data
+subject_pright_error=null    : blob   # confidence interval for precentage went right of data
 subject_delta_fit=null       : blob   # num of right - num of left, x ticks for fitting results
-subject_pright_fit=null      : blob   # (%) fitting results for percent went right
+subject_pright_fit=null      : blob   # fitting results for percent went right
 %}
 
-classdef TowersSubjectPsych < dj.Computed
+classdef TowersSubjectCumulativePsych < dj.Computed
     
-    properties
-        keySource = aggr(subject.Subject, behavior.TowersSession, 'max(session_date)->latest_date') & 'latest_date is not null'
-    end
-
     methods(Access=protected)
         
         function makeTuples(self, key)
 
             deltaBins           = -15:3:15;       % controls binning of #R - #L
             deltaBins           = deltaBins(:);
+           
             
-            [numTowersR, numTowersL, choices] = fetchn(behavior.TowersSession & key, 'num_towers_r', 'num_towers_l', 'chosen_side');
+            [numTowersR, numTowersL, choices] = fetchn(...
+                behavior.TowersSession & sprintf('session_date < "%s"', key.session_date), ...
+                'num_towers_r', 'num_towers_l', 'chosen_side');
             
             numTowersR = cat(2, numTowersR{:});
             numTowersL = cat(2, numTowersL{:});
@@ -35,8 +33,7 @@ classdef TowersSubjectPsych < dj.Computed
             for i = 1:length(f)
                key.(strcat('subject_', f{i})) = fit_results.(f{i});
             end
-
-            key.latest_date = fetch1(self.keySource & key, 'latest_date');
+            
             self.insert(key)
         end
     end
--- scripts/populate_tables.m
+++ scripts/populate_tables.m
@@ -2,4 +2,4 @@
 [keys_towers_session, errors_towers_session] = populate(behavior.TowersSession);
 [keys_block, errors_block] = populate(behavior.TowersBlock);
 [keys_block, errors_block] = populate(behavior.TowersSessionPsych);
-[keys_block, errors_block] = populate(behavior.TowersSubjectPsych);
+[keys_block, errors_block] = populate(behavior.TowersSubjectCumulativePsych);
