4ddf6c27cd5de4d75bd9899bb1cade1dd1669dad
shenshan <shenshanpku@gmail.com>
2021-01-30 00:19:22 -0800

    Add Towers level psych curve to cron job

--- scripts/populate_tables.m
+++ scripts/populate_tables.m
@@ -3,4 +3,4 @@
 [keys_block, errors_block] = populate(behavior.TowersBlock);
 [keys_session_psych, errors_session_psych] = populate(behavior.TowersSessionPsych);
 [keys_subject_psych, errors_subject_psych] = populate(behavior.TowersSubjectCumulativePsych);
-[keys_psych_level, errors_psych_level] = populate(behavior.TowersSubjectCumulativePsych);
+[keys_psych_level, errors_psych_level] = populate(behavior.TowersSubjectCumulativePsychLevel);
