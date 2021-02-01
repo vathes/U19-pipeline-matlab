c9d27098ee584399dbb7652550899093aed314d6
Alvaro Luna <alvalunasan@gmail.com>
2021-02-01 10:37:03 -0500

    Change from BLOB to varchar on task and manipulationtype tables

--- schemas/+task/Task.m
+++ schemas/+task/Task.m
@@ -2,17 +2,17 @@
 task:                   varchar(32)
 ---
 task_description='':    varchar(512)
-table_population='':    BLOB                # list of main tables the task will be populated
+table_population='':    varchar(512)         # list of main tables the task will be populated
 %}
 
 classdef Task < dj.Lookup
     properties
         contents = {
-            'AirPuffs',               '', {'PuffsSession'}
-            'Towers',                 '', {'TowersSession'}
-            'Towers_LickResponse',    '', {'TowersSession', 'TowersBlockTrialLicks'}
-            'Clicks',                 '', {''}
-            'LinearTrack',            '', {''}
+            'AirPuffs',               '', 'PuffsSession'
+            'Towers',                 '', 'TowersSession'
+            'Towers_LickResponse',    '', 'TowersSession, TowersBlockTrialLicks'
+            'Clicks',                 '', ''
+            'LinearTrack',            '', ''
             }
     end
 end
