b270d66e386c2b9de492dddd280eff276087f16c
Alvaro Luna <alvalunasan@gmail.com>
2021-03-05 13:48:40 -0500

    Modified session_error_Log to store complete mexception info

--- schemas/+acquisition/SessionErrorLog.m
+++ schemas/+acquisition/SessionErrorLog.m
@@ -1,11 +1,11 @@
 %{
-# General information of a session
+# Information generated when a session fails
 error_log_id                : int AUTO_INCREMENT            # error identifier
 ---
 -> acquisition.SessionStarted
 session_phase               : enum('Runtime', 'Population') # at which phase of the experiment error ocurred
 error_message               : varchar(512)                  # message of error in matlab  
-error_stack                 : BLOB                          # set of functions where error occured
+error_exception             : BLOB                          # mexception structure with error information
 %}
 
 classdef SessionErrorLog < dj.Manual
