30b59fa8949a2f769062025f489ee7d538bcb718
Alvaro Luna <alvalunasan@gmail.com>
2021-03-29 11:14:00 -0400

    Apparaently even 20 workers are too much for spock/scotty, change to 16

--- schemas/+imaging/Segmentation.m
+++ schemas/+imaging/Segmentation.m
@@ -70,7 +70,7 @@ classdef Segmentation < dj.Imported
       if isempty(gcp('nocreate'))
           
           c = parcluster('local'); % build the 'local' cluster object
-          num_workers = min(c.NumWorkers, 20);
+          num_workers = min(c.NumWorkers, 16);
 
           parpool('local', num_workers, 'IdleTimeout', 120);
       end
