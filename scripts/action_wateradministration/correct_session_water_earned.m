016ba27ccc788ff39f3945b5a0e6734b96c8d079
Alvaro Luna <alvalunasan@gmail.com>
2021-04-02 16:50:32 -0400

    Fix another typo for script water correction

--- scripts/action_wateradministration/correct_session_water_earned.m
+++ scripts/action_wateradministration/correct_session_water_earned.m
@@ -6,11 +6,11 @@ water_admin_info = proj(action.WaterAdministration, 'administration_date->sessio
 
 
 final_comparison = (reward_session * water_admin_info);
-final_comparison2 = proj(final_comparison, 'abs(reward_session-earned)->diff');
+final_comparison2 = proj(final_comparison, 'abs(reward_session-earned)->diff', 'reward_session', 'earned');
 
 extra_info = final_comparison2 * proj(acquisition.SessionStarted, 'remote_path_behavior_file');
 
-session_struct = fetch( extra_info & 'diff>0.05', 'remote_path_behavior_file','ORDER BY session_date desc');
+session_struct = fetch( extra_info & 'diff>0.05', 'remote_path_behavior_file', 'reward_session', 'earned', 'ORDER BY session_date desc')
                     
                     
                    
@@ -24,16 +24,13 @@ for j=1:length(session_struct)
     
     [~, acqsession_file] = lab.utils.get_path_from_official_dir(session_struct(j).remote_path_behavior_file);
     
-    %Load behavioral file
+    %Load behavioral file & update water eanred
     try
         data = load(acqsession_file,'log');
         log = data.log;
+        updateWaterEarnedFromFile(action.WaterAdministration, key, log);
     catch err 
         disp('Could not open file');
     end
-    
-    %updateSessionFromFile_Towers(acquisition.Session,key,log);
-    updateWaterEarnedFromFile(action.WaterAdministration, key, log);
-    
     
 end
