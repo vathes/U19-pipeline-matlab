be96c5d9683451298f80d94dc182ebccd4f99c6c
Alvaro Luna <alvalunasan@gmail.com>
2021-04-06 18:32:49 -0400

    Insert SessionManipulation & OptogeneticSession

--- schemas/+acquisition/SessionManipulation.m
+++ schemas/+acquisition/SessionManipulation.m
@@ -6,5 +6,23 @@
 %}
 
 classdef SessionManipulation < dj.Manual
-  
+    
+     methods
+        
+        function insertSessionManipulation(self,key,log)
+            % Insert session manipulation record from behavioralfile in towersTask
+            % Called at the end of training or when populating session
+            % Input
+            % self         = acquisition.Session instance
+            % key          = structure with required fields: (subject_fullname, date, session_no)
+            % log          = behavioral file as stored in Virmen
+            
+            if ~contains(log.animal.manipulationType, 'none')
+                key.manipulation_type = log.animal.manipulationType;
+                insert(self, key, 'IGNORE');
+            end
+                      
+        end
+    
+     end
 end
--- schemas/+optogenetics/OptogeneticSoftwareParameter.m
+++ schemas/+optogenetics/OptogeneticSoftwareParameter.m
@@ -1,6 +1,6 @@
 %{
 # Parameters related to stimulation control by software during session
-software_parameter_set_id        : INT
+software_parameter_set_id        : INT AUTO_INCREMENT
 ---
 software_parameter_description='': varchar(256)     # string that describes parameter set
 software_parameter_hash          : UUID             # uuid hash that encodes parameter dictionary
