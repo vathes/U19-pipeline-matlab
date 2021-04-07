0d8e0eb60b8e28faa6306356aa199856be1f5446
Alvaro Luna <alvalunasan@gmail.com>
2021-04-07 10:56:04 -0400

    Just check towers sessions not puff task filepaths

--- scripts/acquisition_sessionstarted/check_sessions_filepath.m
+++ scripts/acquisition_sessionstarted/check_sessions_filepath.m
@@ -16,22 +16,26 @@ for j=1:length(session_struct)
     filename = session_struct(j).remote_path_behavior_file;
     filename = strrep(filename, '/mnt/bucket', '');
     
-    if ismac
-        acqsession_file = ['/Volumes' filename];
-    elseif isunix
-        acqsession_file = ['/mnt/bucket' filename];
-    end
-    
-    %[~, acqsession_file] = lab.utils.get_path_from_official_dir(session_struct(j).remote_path_behavior_file);
-    
-    %Load behavioral file & update water eanred
-    
-    if ~exist(acqsession_file, 'file')
+    if ~contains(filename, '/jukebox/')
         
-        num_diff_sessions = num_diff_sessions + 1
+        if ismac
+            acqsession_file = ['/Volumes' filename];
+        elseif isunix
+            acqsession_file = ['/mnt/bucket' filename];
+        end
         
-        aux_key = session_struct(j);
-        session_diff_struct(num_diff_sessions) = aux_key;
+        %[~, acqsession_file] = lab.utils.get_path_from_official_dir(session_struct(j).remote_path_behavior_file);
+        
+        %Load behavioral file & update water eanred
+        
+        if ~exist(acqsession_file, 'file')
+            
+            num_diff_sessions = num_diff_sessions + 1
+            
+            aux_key = session_struct(j);
+            session_diff_struct(num_diff_sessions) = aux_key;
+        end
+        
     end
     
     
