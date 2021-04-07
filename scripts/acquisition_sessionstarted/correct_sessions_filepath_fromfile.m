d832dad6b60c35f235d54d3d980b076b1d8548c7
Alvaro Luna <alvalunasan@gmail.com>
2021-04-07 10:32:20 -0400

    Correct bug on script to correct filepaths

--- scripts/acquisition_sessionstarted/correct_sessions_filepath_fromfile.m
+++ scripts/acquisition_sessionstarted/correct_sessions_filepath_fromfile.m
@@ -23,34 +23,38 @@ for j=1:length(session_diff_struct)
         
         c1 = regexp(filename,'/data/');
         
-        c2 = c1 + length('/data/');
-        c3 = strfind(filename(c2:end), '/');
-        c3 = c3(1)-1;
-        
-        subj_name = lower(filename(c2:c2+c3-1));
-        
-        
-        filename = [filename(1:c2-1)  subj_name filename(c2+c3:end)];
-        
-        if ismac
-            acqsession_file = ['/Volumes' filename];
-        elseif isunix
-            acqsession_file = ['/mnt/bucket' filename];
-        end
-        
-        if ~exist(acqsession_file, 'file')
+        if ~isempty(c1)
             
-            num_diff_sessions = num_diff_sessions + 1
+            c2 = c1 + length('/data/');
+            c3 = strfind(filename(c2:end), '/');
+            c3 = c3(1)-1;
             
-            aux_key = session_diff_struct(j);
-            session_diff_struct2(num_diff_sessions) = aux_key;
-        else
-            key = struct();
-            key.subject_fullname = session_diff_struct(j).subject_fullname;
-            key.session_date = session_diff_struct(j).session_date;
-            key.session_number = session_diff_struct(j).session_number;
+            subj_name = lower(filename(c2:c2+c3-1));
             
-            update(acquisition.SessionStarted & key, 'remote_path_behavior_file', filename);
+            
+            filename = [filename(1:c2-1)  subj_name filename(c2+c3:end)];
+            
+            if ismac
+                acqsession_file = ['/Volumes' filename];
+            elseif isunix
+                acqsession_file = ['/mnt/bucket' filename];
+            end
+            
+            if ~exist(acqsession_file, 'file')
+                
+                num_diff_sessions = num_diff_sessions + 1
+                
+                aux_key = session_diff_struct(j);
+                session_diff_struct2(num_diff_sessions) = aux_key;
+            else
+                key = struct();
+                key.subject_fullname = session_diff_struct(j).subject_fullname;
+                key.session_date = session_diff_struct(j).session_date;
+                key.session_number = session_diff_struct(j).session_number;
+                
+                update(acquisition.SessionStarted & key, 'remote_path_behavior_file', filename);
+            end
+            
         end
         
     end
