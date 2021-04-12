af808b1bf1039a5542427fc7ffe77eb711a04d14
Alvaro Luna <alvalunasan@gmail.com>
2021-04-12 13:55:20 -0400

    Correct definition of OptogeneticTrial

--- schemas/+optogenetics/OptogeneticTrial.m
+++ schemas/+optogenetics/OptogeneticTrial.m
@@ -1,35 +1,16 @@
 %{
 # Information of a optogenetic trial
 -> optogenetics.OptogeneticSession
--> behavior.TowersBlockTrial
+-> acquisition.SessionBlockTrial
 ---
-laser_epoch			              : varchar(32)	 # Which epoch of the trial laser was on
-laser_on			              : tinyint	     # 1 if laser was turned on 0 otherwise
-t_laser_on			              : TINYBLOB	 # times when laser was turned on
-t_laser_off			              : TINYBLOB	 # times when laser was turned off
+stim_epoch			              : varchar(32)	 # Which epoch of the trial stimulation was on
+stim_on			                  : tinyint	     # 1 if stimulation was turned on 0 otherwise
+t_stim_on			              : TINYBLOB	 # times when laser was turned on
+t_stim_off			              : TINYBLOB	 # times when laser was turned off
 %}
 
-stim_epoch
 
--reward
--cue
--cue+reward
-
-
-stim instead of laser
-
-PhotostimTrial
-
-add -> Photostim (waverform etc for a specific event of stimulation)
-
-+ part table of OptogeneticTrial
-
-e.g OptogeneticTrialEvent
-    Event 
-
-
-
-classdef OptogeneticTrial < dj.Manual
+classdef OptogeneticTrial < dj.Imported
     methods(Access=protected)
         function makeTuples(self, key)
             
