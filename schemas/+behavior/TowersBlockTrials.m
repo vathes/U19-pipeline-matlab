1af85b2974626656a7c5057a8cd759f471a86864
Alvaro Luna <alvalunasan@gmail.com>
2021-04-13 22:50:20 -0400

    TowersBlockTrials ingestion corrected

--- schemas/+behavior/TowersBlockTrials.m
+++ schemas/+behavior/TowersBlockTrials.m
@@ -5,8 +5,8 @@
 trial_type:                 enum('L', 'R')          # answer of this trial, left or right
 choice:                     enum('L', 'R', 'nil')   # choice of this trial, left or right
 trial_abs_start:            float                   # absolute start time of the trial realtive to the beginning of the session
-cue_presence_left:          blob                    # boolean vector for the presence of the towers on the left
-cue_presence_right:         blob                    # boolean vector for the presence of the towers on the right
+cue_presence_left=null:     blob                    # boolean vector for the presence of the towers on the left
+cue_presence_right=null:    blob                    # boolean vector for the presence of the towers on the right
 cue_onset_left=null:        blob                    # onset time of the cues on the left (only for the present ones)
 cue_onset_right=null:       blob                    # onset time of the cues on the right (only for the present ones)
 cue_offset_left=null:       blob                    # offset time of the cues on the left (only for the present ones)
@@ -21,16 +21,16 @@ i_cue_entry:                int                     # the index of the time seri
 i_mem_entry:                int                     # the index of the time series when the mouse enters the memory zone
 i_turn_entry:               int                     # the index of the time series when the mouse enters turns
 iterations:                 int                     # length of the meaningful recording
-trial_time:                 longblob@extstorage     # time series of this trial, start from zero for each trial
-collision:                  longblob@extstorage     # boolean vector indicating whether the subject hit the maze on each time point
-position:                   longblob@extstorage     # 3d recording of the position of the mouse, length equals to interations
-velocity:                   longblob@extstorage     # 3d recording of the velocity of the mouse, length equals to interations
-sensor_dots:                longblob@extstorage     # raw recordings of the ball
 trial_id:                   int                     #
 trial_prior_p_left:         float                   # prior probablity of this trial for left
 vi_start:                   int                     #
+trial_time:                 blob@extstorage         # time series of this trial, start from zero for each trial
+collision:                  blob@extstorage         # boolean vector indicating whether the subject hit the maze on each time point
+position:                   blob@extstorage         # 3d recording of the position of the mouse, length equals to interations
+velocity:                   blob@extstorage         # 3d recording of the velocity of the mouse, length equals to interations
+sensor_dots:                blob@extstorage         # raw recordings of the ball
 %}
-
+ 
 classdef TowersBlockTrials < dj.Part
     properties(SetAccess=protected)
         master = behavior.TowersBlock
@@ -52,7 +52,11 @@ classdef TowersBlockTrials < dj.Part
                 
                 %Get trial information and insert
                 trial_info = fetch(behavior.TowersBlockTrial & all_sessions(i), '*');
-                insert(self, trial_info, 'IGNORE');
+                tic
+                if ~isempty(trial_info)
+                    insert(self, trial_info, 'IGNORE');
+                end
+                toc
                 
             end
             warning('on','MATLAB:MKDIR:DirectoryExists')
@@ -61,3 +65,5 @@ classdef TowersBlockTrials < dj.Part
         
         
     end
+    
+end
