24b365a1b4bf8958c3d16250d583346f49fc5029
Alvaro Luna <alvalunasan@gmail.com>
2021-04-21 11:01:01 -0400

    ingestFromBehaviorBlockTrial also for BlockTrialOld table (to catch up)

--- schemas/+behavior/TowersBlockTrialOld.m
+++ schemas/+behavior/TowersBlockTrialOld.m
@@ -16,11 +16,11 @@ cue_offset_right=null:      blob     # offset time of the cues on the right (onl
 cue_pos_left=null:          blob     # position of the cues on the left (only for the present ones)
 cue_pos_right=null:         blob     # position of the cues on the right (only for the present ones)
 trial_duration:             float    # duration of the entire trial
-excess_travel:              float    # 
+excess_travel:              float    #
 i_arm_entry:                int      # the index of the time series when the mouse enters the arm part
 i_blank:                    int      # the index of the time series when the mouse enters the blank zone
 i_cue_entry:                int      # the index of the time series when the mouse neters the cue zone
-i_mem_entry:                int      # the index of the time series when the mouse enters the memory zone 
+i_mem_entry:                int      # the index of the time series when the mouse enters the memory zone
 i_turn_entry:               int      # the index of the time series when the mouse enters turns
 iterations:                 int      # length of the meaningful recording
 position:                   longblob # 3d recording of the position of the mouse, length equals to interations
@@ -28,11 +28,40 @@ velocity:                   longblob # 3d recording of the velocity of the mouse
 sensor_dots:                longblob # raw recordings of the ball
 trial_id:                   int      #
 trial_prior_p_left:         float    # prior probablity of this trial for left
-vi_start:                   int      # 
+vi_start:                   int      #
 %}
 
 classdef TowersBlockTrialOld < dj.Part
     properties(SetAccess=protected)
         master = behavior.TowersBlock
     end
+    
+    methods
+        
+        function ingestFromBehaviorBlockTrial(self)
+            %Function to copy over trials from behavior.TowersBlockTrial -> behavior.TowersBlockTrials
+            
+            all_sessions = fetch(acquisition.Session - self);
+            
+            %For all sessions
+            num_sessions = length(all_sessions);
+            for i=1:num_sessions
+                
+                [i num_sessions]
+                
+                %Get trial information and insert
+                trial_info = fetch(behavior.TowersBlockTrial & all_sessions(i), '*');
+                tic
+                if ~isempty(trial_info)
+                    insert(self, trial_info, 'IGNORE');
+                end
+                toc
+                
+            end
+            
+        end
+        
+        
+    end
+    
 end
