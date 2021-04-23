67ab9e01862157e873ecf6a25ec2cf123a6cd545
shenshan <shenshanpku@gmail.com>
2021-04-23 02:33:25 -0400

    generate test meta data from production database

--- scripts/generate_test_data.m
+++ scripts/generate_test_data.m
@@ -7,16 +7,19 @@ session_started_data = fetch(acquisition.SessionStarted & session_key, '*');
 session_data = fetch(session_query, '*');
 
 task_data = fetch(task.Task & session_started_query, '*')
-task_level_parameter_set_data = fetch(task.TaskLevelParameterSet & session_query, '*');
-location_session_data = fetch1(lab.Location & proj(session_query, 'session_location->location'), 'x');
-
+task_level_parameter_set_data = fetch(task.TaskLevelParameterSet, '*');
+location_session_data = fetch(lab.Location & proj(session_query, 'session_location->location'), '*');
 
 subject_query = subject.Subject & session_query;
 subject_data = fetch(subject_query, '*');
 line_data = fetch(subject.Line & subject_query, '*');
-location_subject_data = fetch1(lab.Location & subject_query, '*');
-protocol_subject_data = fetch1(lab.Protocol & subject_query, '*');
+location_subject_data = fetch(lab.Location & subject_query, '*');
+protocol_subject_data = fetch(lab.Protocol & subject_query, '*');
 user_subject_data = fetch(lab.User & subject_query, '*');
 
-lab_data = fetch1(lab.Lab & 'lab="tank_lab"', '*');
-location_data = fetch1(lab.Location & )
+lab_data = fetch(lab.Lab & 'lab="tanklab"', '*');
+acquisition_type_data = fetch(lab.AcquisitionType, '*')
+
+path_data = fetch(lab.Path, '*')
+
+save([dj.config('root_dir'), '/tests/test_data/testmeta.mat'])
