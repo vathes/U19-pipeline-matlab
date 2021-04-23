c1d479150bb05faba669a5aa685e4b86de6565ed
shenshan <shenshanpku@gmail.com>
2021-04-23 13:22:33 -0400

    bug fixes in populate routine

--- scripts/populate_tables.m
+++ scripts/populate_tables.m
@@ -1,4 +1,4 @@
-[keys_dir, errors_dir] = populate(behavior.DataDirectory, session_date>"2021-01-01");
+[keys_dir, errors_dir] = populate(behavior.DataDirectory, 'session_date>"2021-01-01"');
 [keys_session_block, errors_session_block] = populate(acquisition.SessionBlock)
 [keys_towers_session, errors_towers_session] = populate(behavior.TowersSession);
 [keys_block, errors_block] = populate(behavior.TowersBlock);
