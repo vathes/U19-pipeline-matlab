cd67f61d16755d1b05e337eb75caa20d5a6fd625
shenshan <shenshanpku@gmail.com>
2021-04-23 02:34:44 -0400

    Add SessionBlock into autopoulate process

--- scripts/populate_tables.m
+++ scripts/populate_tables.m
@@ -1,4 +1,5 @@
-[keys_dir, errors_dir] = populate(behavior.DataDirectory);
+[keys_dir, errors_dir] = populate(behavior.DataDirectory, session_date>"2021-01-01");
+[keys_session_block, errors_session_block] = populate(acquisition.SessionBlock)
 [keys_towers_session, errors_towers_session] = populate(behavior.TowersSession);
 [keys_block, errors_block] = populate(behavior.TowersBlock);
 [keys_session_psych, errors_session_psych] = populate(behavior.TowersSessionPsych);
