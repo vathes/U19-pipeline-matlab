6296da019a57c25208af7bdad7f6f081a21c56d1
shenshan <shenshanpku@gmail.com>
2021-04-23 02:35:10 -0400

    Test suite for populate procedures.

--- tests/Prep.m
+++ tests/Prep.m
@@ -2,10 +2,11 @@ classdef Prep < matlab.unittest.TestCase
     % Setup and teardown for tests.
     properties (Constant)
         CONN_INFO = struct(...
-            'host', getenv('DJ_HOST'), ...
-            'user', getenv('DJ_USER'), ...
-            'password', getenv('DJ_PASS'));
+            'host', dj.config('databaseHost'), ...
+            'user', dj.config('databaseUser'), ...
+            'password', dj.config('databasePassword'));
         PREFIX = 'u19_test_';
+        test_root = dj.config('root_dir');
     end
     methods (TestClassSetup)
         function init(testCase)
@@ -15,6 +16,39 @@ classdef Prep < matlab.unittest.TestCase
             curr_conn = dj.conn(testCase.CONN_INFO.host, ...
                 testCase.CONN_INFO.user, testCase.CONN_INFO.password,'',true);
             setenv('DB_PREFIX', testCase.PREFIX)
+
+            disp('----------Creating schemas--------');
+            dj.createSchema('lab', [testCase.test_root '/schemas'], ...
+                [testCase.PREFIX 'lab']);
+
+            dj.createSchema('task', [testCase.test_root, '/schemas'], ...
+                [testCase.PREFIX 'task']);
+
+            dj.createSchema('subject', [testCase.test_root, '/schemas'], ...
+                [testCase.PREFIX 'subject']);
+
+            dj.createSchema('acquisition', [testCase.test_root, '/schemas'], ...
+                [testCase.PREFIX 'acquisition']);
+
+            dj.createSchema('behavior', [testCase.test_root, '/schemas'], ...
+                [testCase.PREFIX 'behavior']);
+
+            load([testCase.test_root '/tests/test_data/testmeta.mat'])
+
+            inserti(lab.Lab, lab_data)
+            inserti(lab.Path, path_data)
+            inserti(lab.AcquisitionType, acquisition_type_data)
+            inserti(lab.Location, location_subject_data)
+            inserti(lab.Location, location_session_data)
+            inserti(lab.Protocol, protocol_subject_data)
+            inserti(lab.User, user_subject_data)
+            inserti(task.Task, task_data)
+            inserti(task.TaskLevelParameterSet, task_level_parameter_set_data)
+            inserti(subject.Line, line_data)
+            inserti(subject.Subject, subject_data)
+            inserti(acquisition.SessionStarted, session_started_data)
+            inserti(acquisition.Session, session_data)
+
         end
     end
     methods (TestClassTeardown)
@@ -26,15 +60,13 @@ classdef Prep < matlab.unittest.TestCase
 
             % remove databases
             curr_conn.query('SET FOREIGN_KEY_CHECKS=0;');
-            res = curr_conn.query(['SHOW DATABASES LIKE "' testCase.PREFIX '_%";']);
-            for i = 1:length(res.(['Database (' testCase.PREFIX '_%)']))
+            res = curr_conn.query(['SHOW DATABASES LIKE "' testCase.PREFIX '%";']);
+            for i = 1:length(res.(['Database (' testCase.PREFIX '%)']))
                 curr_conn.query(['DROP DATABASE ' ...
-                    res.(['Database (' testCase.PREFIX '_%)']){i} ';']);
+                    res.(['Database (' testCase.PREFIX '%)']){i} ';']);
             end
             curr_conn.query('SET FOREIGN_KEY_CHECKS=1;');
-
-            res = curr_conn.query(sprintf('%s',cmd{:}));
-            curr_conn.delete;
+            curr_conn.close();
         end
     end
 end
--- tests/TestBehaviorPopulate.m
+++ tests/TestBehaviorPopulate.m
@@ -1,55 +1,13 @@
 classdef TestBehaviorPopulate < Prep
     methods(Test)
         function TestBehaviorPopulate_testPopulate(testCase)
-            st = dbstack;
-            disp(['---------------' st(1).name '---------------']);
-            package = 'Lab';
 
-            dj.createSchema(package,[testCase.test_root '/test_schemas'], ...
-                [testCase.PREFIX '_lab']);
+            populate(acquisition.SessionBlock)
+            populate(behavior.TowersSession)
+            populate(behavior.TowersBlock)
 
-            lab_schema = Lab.getSchema; % we need schema's connection id
-            sid = lab_schema.conn.serverId;
-
-            insert(Lab.Subject, {
-               100, '2010-04-02';
-            });
-
-            insert(Lab.Rig, struct( ...
-                'rig_manufacturer', 'FooLab', ...
-                'rig_model', '1.0', ...
-                'rig_note', 'FooLab Frobnicator v1.0' ...
-            ));
-
-            % parallel populate of 1 record
-            % .. (SessionAnalysis logs session ID as session_analysis data)
-            % NOTE: need to call parpopulate 1st to ensure Jobs table
-            % exists
-
-            insert(Lab.Session, struct( ...
-                'session_id', 0, ...
-                'subject_id', 100, ...
-                'rig_manufacturer', 'FooLab', ...
-                'rig_model', '1.0' ...
-            ));
-
-            parpopulate(Lab.SessionAnalysis);
-            a_result = fetch(Lab.SessionAnalysis & 'session_id = 0', '*');
-            testCase.verifyEqual(a_result.session_analysis.connection_id, sid);
-
-            % regular populate of 1 record
-            % .. (SessionAnalysis logs jobs record as session_analysis data)
-
-            insert(Lab.Session, struct( ...
-                'session_id', 1, ...
-                'subject_id', 100, ...
-                'rig_manufacturer', 'FooLab', ...
-                'rig_model', '1.0' ...
-            ));
-
-            populate(Lab.SessionAnalysis);
-            a_result = fetch(Lab.SessionAnalysis & 'session_id = 1', '*');
-            testCase.verifyEqual(a_result.session_analysis, 1);
+            testCase.verifyEqual(length(fetch(behavior.TowersBlock)), 12);
+            testCase.verifyEqual(length(fetch(behavior.TowersBlockTrial)), 420)
 
         end
     end
