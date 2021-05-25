304d7d3fd1714f5e0f3611976e9505e3f163ea69
shenshan <shenshanpku@gmail.com>
2021-05-25 19:43:09 -0400

    Add behavior.TowersSubjectCumulativePsychTask to daily cron jobs.

--- scripts/populate_tables.m
+++ scripts/populate_tables.m
@@ -5,3 +5,4 @@
 [keys_session_psych, errors_session_psych] = populate(behavior.TowersSessionPsych);
 [keys_subject_psych, errors_subject_psych] = populate(behavior.TowersSubjectCumulativePsych);
 [keys_psych_level, errors_psych_level] = populate(behavior.TowersSubjectCumulativePsychLevel);
+[keys_psych_task, errors_psych_task] = populate(behavior.TowersSubjectCumulativePsychTask);
